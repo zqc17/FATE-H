ba7e4c0c9d763cbe0ceee2371bdba430bea41e20
zqc17 <zhangqinchuan17@163.com>
2025-09-30 13:53:37 +0800

    fix: $SL_2(F_3)/Z(SL_2(F_3))$ should be isomorphic to $A_4$.

--- FATEH/12.lean
+++ FATEH/12.lean
@@ -3,9 +3,8 @@ import Mathlib
 open MatrixGroups
 
 /--
-Prove that \( SL_2(\mathbb{F}_3) / Z(SL_2(\mathbb{F}_3)) < A_4 \).
+Prove that \( SL_2(\mathbb{F}_3) / Z(SL_2(\mathbb{F}_3)) \cong A_4 \).
 -/
 theorem exists_sl_quot_center_monoidHom_alternatingGroup :
-    ∃ φ : SL(2,ZMod 3) ⧸ Subgroup.center SL(2,ZMod 3) →* alternatingGroup (Fin 4),
-    Function.Injective φ := by
+    Nonempty (SL(2,ZMod 3) ⧸ Subgroup.center SL(2,ZMod 3) ≃* alternatingGroup (Fin 4)) := by
   sorry
